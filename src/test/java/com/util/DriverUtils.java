3d9435eb17c7eec3bb17019804a6106b32de6e8a
Sree Muduru <sreemuduru@users.noreply.github.com>
2017-10-22 11:24:52 -0400

    Scrolling logic

--- src/test/java/com/util/DriverUtils.java
+++ src/test/java/com/util/DriverUtils.java
@@ -1,7 +1,9 @@
 package com.util;
 
 import com.config.GlobalConfiguration;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -20,6 +22,10 @@ public class DriverUtils {
 
     @Autowired
     protected ApplicationEventPublisher eventPublisher;
+
+    public static final String SCROLL_ELEMENT_VERTICAL = "arguments[0].scrollTop + arguments[i]";
+
+    public static final String SCROLL_ELEMENT_HOTIZONTAL = "arguments[0].scrollLeft + arguments[i]";
 
     public void waitSecs(long secs){
         waitMillis(secs * 1000);
@@ -43,6 +49,11 @@ public class DriverUtils {
 
     public WebDriver getWebDriver(){
         return WebDriverRunner.getWebDriver();
+    }
+
+    public void scrollElementVertical(WebElement el, int scrollIncrement){
+        JavascriptExecutor js = (JavascriptExecutor) getWebDriver();
+        js.executeScript(SCROLL_ELEMENT_VERTICAL, new Integer((scrollIncrement)));
     }
 
 }
